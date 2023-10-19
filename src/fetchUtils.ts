1f0018e6fad42dbfe374cc59f6d8f188dc1fe17d
cachethemall <kieuphuongduong@gmail.com>
2023-10-19 14:14:24 +0700

    Fix delay fetch

--- src/fetchUtils.ts
+++ src/fetchUtils.ts
@@ -1,20 +1,15 @@
+
 const requestQueue = {}; // This will store the request queues for each domain
 
+function processQueue(domain: string) {
+
+  const callback = requestQueue[domain][0];
 
 
-function processQueue(domain) {
-  let callback;
-  try {
-    callback = requestQueue[domain][0];
-  } catch (err) {
-    debugger;
-    return;
-  }
-  
-  
+
   if (callback) {
     fetch(callback.url, callback.options)
-    //   .then(response => response.json())
+      //   .then(response => response.json())
       .then(data => {
         callback.resolve(data);
       })
@@ -27,26 +22,54 @@ function processQueue(domain) {
           processQueue(domain);
         }, 2000); // Add a 1-second delay between requests to the same domain
       });
-  } 
-  else {
+  } else {
+    // removeItemOnce(currentDomainsInQueue, domain);
     delete requestQueue[domain];
   }
 }
 
 export function fetchWithDelay(url, options?): Promise<any> {
   const domain = new URL(url).hostname;
-  
+
   return new Promise((resolve, reject) => {
-    addToQueue(domain, { url, options, resolve, reject });
+    const callback = { url, options, resolve, reject };
+
+    if (requestQueue.hasOwnProperty(domain)) {
+      requestQueue[domain].push(callback);
+    } else {
+      requestQueue[domain] = [callback];
+      processQueue(domain);
+    }
+
+
+    // if (!currentDomainsInQueue.includes(domain)) {
+    //   currentDomainsInQueue.push(domain);
+    //   processQueue(domain);
+    // }
+    // else {
+    //   console.log(currentDomainsInQueue);
+    // }
+
   });
 }
 
-function addToQueue(domain, callback) {
-  if (!requestQueue[domain]) {
-    requestQueue[domain] = [];
+function removeItemOnce(arr, value) {
+  var index = arr.indexOf(value);
+  if (index > -1) {
+    arr.splice(index, 1);
   }
-  requestQueue[domain].push(callback);
-  if (requestQueue[domain].length === 1) {
-    processQueue(domain);
-  }
+  return arr;
 }
+
+function removeItemAll(arr, value) {
+  var i = 0;
+  while (i < arr.length) {
+    if (arr[i] === value) {
+      arr.splice(i, 1);
+    } else {
+      ++i;
+    }
+  }
+  return arr;
+}
+
--- src/main.tsx
+++ src/main.tsx
@@ -26,4 +26,3 @@ reactRoot.render(
 //   cc++;
 // }, 1000);
 
-
